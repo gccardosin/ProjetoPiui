74f75c8bc4b1e100a92ebd2a4c3aa577cea944c3
Guilherme Cardoso <86062682+gccardosin@users.noreply.github.com>
2022-09-19 23:00:19 -0300

    Add files via upload

---
+++ ProjetoPiui/main.js
@@ -0,0 +1,75 @@
+// DOM Document Object Model
+
+/**Toggle Menu, abre e fecha o menu ao clicar no icone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+//adiciona o show ou tira quando necessario
+for (const div of toggle) {
+  div.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+/**quando clicar em um item o menu fecha */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
+
+/**mudar o header da pagina quando scroll ¨Sombra¨ */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+/**Testimonials slider */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+
+  mousewhell: true,
+  keyboard: true
+})
+
+/**ScrollReveal: mostrar elementos quando der scroll na pag */
+
+/*const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 750,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)*/
+
+/** Back to top button */
+
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
